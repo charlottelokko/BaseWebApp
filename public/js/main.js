55dec8b4d26fdf144579d30c30f47109c761005f
Charlie L <littlelokko@hotmail.com>
2017-11-10 16:40:06 +0000

    updating

--- public/js/main.js
+++ public/js/main.js
@@ -8,4 +8,57 @@ function myFunction() {
   // 'img-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
   $("#doge-image").append(`<img class="img-circle" src="/images/wowdoge.jpeg" />`);
 }
+function addMessage(body, title) {
+  var user = firebase.auth().currentUser;
+  var authorPic = user.photoURL;
+  var author = user.displayName;
 
+  var postData = {
+    author: author,
+    authorPic: authorPic,
+    title: title,
+    body: body
+  };
+
+  var newPostKey = firebase.database().ref().child('stream').push().key;
+  firebase.database().ref('/stream/' + newPostKey).set(postData);
+}
+
+function handleMessageFormSubmit() {
+  var body = $('#new-post-body').val();
+  var title = $('#new-post-title').val();
+
+  addMessage(body, title);
+}
+
+function toggleSignIn() {
+  if (!firebase.auth().currentUser) { // if no user, handle login
+    var provider = new firebase.auth.GoogleAuthProvider();
+    provider.addScope('https://www.googleapis.com/auth/plus.login');
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+      console.log("success");
+    }).catch(function(error) {
+      console.error("error", error);
+    });
+  } else { // handle logout
+    firebase.auth().signOut();
+  }
+
+  //This disables the button until login or logout is successful
+  $('#login-button').attr("disabled", true);
+}
+
+
+window.onload = function() {
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      $('#stream').show();
+      $('#login-button').html("Log out");
+      initializeStreamListener();
+    } else {
+      $('#stream').hide();
+      $('#login-button').html("Log in with google");
+    }
+    $('#login-button').attr("disabled", false);
+  });
+};
