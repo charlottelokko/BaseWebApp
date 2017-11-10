ba28c1c6a50c5a6a42076a35f6b3ebb82ca1de26
Charlie L <littlelokko@hotmail.com>
2017-11-10 17:06:04 +0000

    added comments

--- public/js/main.js
+++ public/js/main.js
@@ -8,6 +8,9 @@ function myFunction() {
   // 'img-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
   $("#doge-image").append(`<img class="img-circle" src="/images/wowdoge.jpeg" />`);
 }
+
+// This function sets up a listener- '.on()' gets called automatically whenever something saved in '/stream/' changes.
+// It's main purpose is to iterate over the stream in the database and add each message to the page.
 function initializeStreamListener() {
   const databaseStreamReference = firebase.database().ref('/stream/');
 
@@ -24,6 +27,8 @@ function initializeStreamListener() {
   });
 }
 
+// This function gets called with the new message information.
+// It gets the user information and uses both to add the post to the database.
 function addMessage(body, title) {
   var user = firebase.auth().currentUser;
   var authorPic = user.photoURL;
@@ -40,6 +45,8 @@ function addMessage(body, title) {
   firebase.database().ref('/stream/' + newPostKey).set(postData);
 }
 
+// This gets called whenver the form is submitted (check out the index.ejs).
+// Uses jQuery to get the message info and passes it to 'addMessage to actually submit the info to firebase.
 function handleMessageFormSubmit() {
   var body = $('#new-post-body').val();
   var title = $('#new-post-title').val();
@@ -47,6 +54,7 @@ function handleMessageFormSubmit() {
   addMessage(body, title);
 }
 
+// Gets called whenever the user clicks "sign in" or "sign out".
 function toggleSignIn() {
   if (!firebase.auth().currentUser) { // if no user, handle login
     var provider = new firebase.auth.GoogleAuthProvider();
@@ -64,7 +72,9 @@ function toggleSignIn() {
   $('#login-button').attr("disabled", true);
 }
 
-
+// The main purpose of this function is to set up a listener (using firebase) for when the auth state changes.
+// If a user isn't authenticated, we should not show the stream and prompt them to log in.
+// If a use IS authenticated, we should load/show the stream and give them the option to log out.
 window.onload = function() {
   firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
